81e3c475673dede4346d9f91222c878a5c2283a3
INEEDAMONITOR <joel.yuxi@gmail.com>
2023-12-02 18:45:00 -0500

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = document.getElementById('member');
+    var memberSkills = document.getElementById('member-skills');
+
+    if (member.checked == true) {
+        memberSkills.style.display = 'block';
+    } else {
+        memberSkills.style.display = 'none';
+    }
+}
